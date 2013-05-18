6a0664f48a9aac40b6edddfa0e01cd4472a42dd5
lociii <opensource@jensnistler.de>
2013-05-18 11:18:19 +0200

    initial checkin

---
+++ README.rst
@@ -0,0 +1,19 @@
+Jukebox mpg123 playback module
+============================
+
+jukebox_mpg123 simply plays the music on your local machine using the cli tool `mpg123 <http://mpg123.de/>`_
+
+Startup
+========
+
+::
+
+    bin/jukebox jukebox_mpg123 --start
+
+Shutdown
+=====
+
+::
+
+    bin/jukebox jukebox_mpg123 --stop
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="jukebox_mpg123",
+    packages=find_packages(),
+    version="0.1",
+    description="mpg123 playback service for jukebox",
+    author="Jens Nistler",
+    author_email="opensource@jensnistler.de",
+    url="http://jensnistler.de/",
+    download_url='http://github.com/lociii/jukebox_mpg123',
+    keywords=["jukebox", "music", "mp3", "playback", "backend", "mpg123"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Sound/Audio :: Players",
+    ],
+    install_requires=[
+        "jukebox>=0.3.7",
+        "python-daemon==1.6",
+    ],
+    include_package_data=True,
+    long_description=open("README.rst").read()
+)
