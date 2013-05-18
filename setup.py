9018dc2a9bbd8cab50b8ae7d31a6096cefb014db
lociii <opensource@jensnistler.de>
2013-05-18 11:34:55 +0200

    rename package to jukebox-mpg123

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="jukebox_mpg123",
+    name="jukebox-mpg123",
     packages=find_packages(),
     version="0.1",
     description="mpg123 playback service for jukebox",
